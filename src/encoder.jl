e71bdb2a885a48fa26a167d69110a81f869167c1
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-08-16 13:11:15 -0300

    Project updates

--- src/encoder.jl
+++ src/encoder.jl
@@ -1,23 +1,23 @@
 function encode(::Type{CamelCase}, v::AbstractVector{<:AbstractString})::String
-    return s[1] * join(titlecase.(s[2:end]))
+    return v[1] * join(titlecase.(v[2:end]))
 end
 
 function encode(::Type{FlatCase}, v::AbstractVector{<:AbstractString})::String
-    return join(lowercase.(s), ' ')
+    return join(lowercase.(v), ' ')
 end
 
 function encode(::Type{KebabCase}, v::AbstractVector{<:AbstractString})::String
-    return join(lowercase.(s), '-')
+    return join(lowercase.(v), '-')
 end
 
 function encode(::Type{PascalCase}, v::AbstractVector{<:AbstractString})::String
-    return join(titlecase.(s))
+    return join(titlecase.(v))
 end
 
 function encode(::Type{ScreamingSnakeCase}, v::AbstractVector{<:AbstractString})::String
-    return join(uppercase.(s), '_')
+    return join(uppercase.(v), '_')
 end
 
 function encode(::Type{SnakeCase}, v::AbstractVector{<:AbstractString})::String
-    return join(lowercase.(s), '_')
+    return join(lowercase.(v), '_')
 end
